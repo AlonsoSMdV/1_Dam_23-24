f6e6b0f967e1348305538d8759472d2337408c10
Alonso <asanmor011004@gmail.com>
2023-10-04 13:27:48 +0200

    Commit

---
+++ CapituloJava01/Ejercicio09.java
@@ -0,0 +1,16 @@
+package CapituloJava01;
+
+public class Ejercicio09 {
+  public static void main(String[] args) {
+    System.out.println("                       ██");
+    System.out.println("  Aprende Java       ██");
+    System.out.println(" con ejercicios        ██");
+    System.out.println("                               ");
+    System.out.println(" Alonso Sánchez      ██████████");
+    System.out.println(" Moreno de Vega      ███████  █");
+    System.out.println("                     ██████████");
+    System.out.println("                     ██████");
+    System.out.println("▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒██████▒▒▒▒▒▒▒▒");
+    System.out.println("▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒");
+  }
+}
---
+++ CapituloJava01/Ejercicio10.java
@@ -0,0 +1,15 @@
+package CapituloJava01;
+
+public class Ejercicio10 {
+  public static void main(String[] args) {
+    System.out.println("                       \033[31m██");
+    System.out.println("  \033[32mAprende Java       \033[31m██");
+    System.out.println(" \033[32mcon ejercicios        \033[31m██");
+    System.out.println("                               ");
+    System.out.println(" \033[33mAlonso Sánchez      \033[94m██████████");
+    System.out.println(" \033[33mMoreno de Vega      \033[94m███████  █");
+    System.out.println("                     \033[94m██████████");
+    System.out.println("\033[34m▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒\033[94m██████\033[34m▒▒▒▒▒▒▒▒");
+    System.out.println("\033[34m▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒");
+  }
+}
