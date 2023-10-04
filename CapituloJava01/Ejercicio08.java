c65e0e07c77ae81ca3f5b6dc4a8aa959ef0f797c
Alonso <asanmor011004@gmail.com>
2023-10-04 13:27:30 +0200

    Commit

---
+++ CapituloJava01/Ejercicio08.java
@@ -0,0 +1,11 @@
+package CapituloJava01;
+
+public class Ejercicio08 {
+  public static void main(String[] args) {
+    System.out.println("*********");
+    System.out.println(" *     *");
+    System.out.println("  *   *");
+    System.out.println("   * *");
+    System.out.println("    *");
+  }
+}
