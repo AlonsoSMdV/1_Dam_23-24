754c93c098aaa3789fa30f8b80cf7ee1b4ac5f88
Alonso <asanmor011004@gmail.com>
2023-10-04 13:25:09 +0200

    Commit

---
+++ CapituloJava01/Ejercicio02.java
@@ -0,0 +1,7 @@
+package CapituloJava01;
+
+public class Ejercicio02{
+  public static void main(String[] args){
+    System.out.print("Alonso Sánchez Moreno de Vega\nC/Eneas,58\n681 63 40 35");
+  }
+}
---
+++ CapituloJava01/Ejercicio03.java
@@ -0,0 +1,16 @@
+package CapituloJava01;
+
+public class Ejercicio03 {
+  public static void main(String[] args){
+    System.out.printf("%-15s %s\n","computer","ordenador");
+    System.out.printf("%-15s %s\n","cat","gato");
+    System.out.printf("%-15s %s\n","book","libro");
+    System.out.printf("%-15s %s\n","red","rojo");
+    System.out.printf("%-15s %s\n","flag","bandera");
+    System.out.printf("%-15s %s\n","machine","máquina");
+    System.out.printf("%-15s %s\n","dog","perro");
+    System.out.printf("%-15s %s\n","light","luz");
+    System.out.printf("%-15s %s\n","circle","círculo");
+    System.out.printf("%-15s %s\n","lion","león");
+  }
+}
