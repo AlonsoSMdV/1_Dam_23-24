703199311b987b2a9a49f2058ae8c536f55a6207
Alonso <asanmor011004@gmail.com>
2023-10-05 10:20:19 +0200

    Enunciado

--- CapituloJava01/Ejercicio07.java
+++ CapituloJava01/Ejercicio07.java
@@ -1,5 +1,8 @@
 package CapituloJava01;
-
+/**
+ * Igual que el programa anterior, pero esta vez la pirámide estará hueca (se debe
+ * ver únicamente el contorno hecho con asteriscos).
+ */
 public class Ejercicio07 {
   public static void main(String[] args) {
     System.out.println("    *");
--- CapituloJava01/Ejercicio08.java
+++ CapituloJava01/Ejercicio08.java
@@ -1,5 +1,8 @@
 package CapituloJava01;
-
+/** 
+ * Igual que el programa anterior, pero esta vez la pirámide debe aparecer
+ * invertida, con el vértice hacia abajo.
+*/
 public class Ejercicio08 {
   public static void main(String[] args) {
     System.out.println("*********");
