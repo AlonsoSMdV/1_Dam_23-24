6813f8af687beb8925a894371570c5fcff349c21
Alonso <asanmor011004@gmail.com>
2023-10-05 10:19:58 +0200

    Enunciado

--- CapituloJava01/Ejercicio01.java
+++ CapituloJava01/Ejercicio01.java
@@ -1,5 +1,7 @@
 package CapituloJava01;
-
+/**
+ * Escribe una programa que muestre tu nombre por pantalla.
+ */
 public class Ejercicio01{
   public static void main(String[] args){
     System.out.print("Alonso Sánchez Moreno de Vega");
--- CapituloJava01/Ejercicio02.java
+++ CapituloJava01/Ejercicio02.java
@@ -1,5 +1,9 @@
 package CapituloJava01;
-
+/**
+ * Modifica el programa anterior para que además se muestre tu dirección y
+ * tu número de teléfono. Asegúrate de que los datos se muestran en líneas
+ * separadas.
+ */
 public class Ejercicio02{
   public static void main(String[] args){
     System.out.print("Alonso Sánchez Moreno de Vega\nC/Eneas,58\n681 63 40 35");
