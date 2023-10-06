34c0788f81530cbd3a9abe3065159eb8d5ea293e
Alonso <asanmor011004@gmail.com>
2023-10-06 17:13:39 +0200

    Commit

---
+++ CapituloJava02/Ejercicio02.java
@@ -0,0 +1,12 @@
+package CapituloJava02;
+/**
+ * Crea la variable nombre y asígnale tu nombre completo. Muestra su valor por
+ * pantalla de tal forma que el resultado del programa sea el mismo que en el
+ * ejercicio 1 del capítulo 1.
+ */
+public class Ejercicio02  {
+  public static void main(String[] args) {
+    String nombre = "Alonso sánchez Moreno de Vega";
+    System.out.println(nombre);
+  }
+}
