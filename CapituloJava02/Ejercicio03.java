32113579abf66adb89ab323cf04c92c1449f6cbc
Alonso <asanmor011004@gmail.com>
2023-10-06 17:13:50 +0200

    Commit

---
+++ CapituloJava02/Ejercicio03.java
@@ -0,0 +1,16 @@
+package CapituloJava02;
+/**
+ * Crea las variables nombre, direccion y telefono y asígnales los valores correspondientes. 
+ * Muestra los valores de esas variables por pantalla de tal forma que elresultado del programa 
+ * sea el mismo que en el ejercicio 2.
+ */
+public class Ejercicio03 {
+  public static void main(String[] args) {
+    String nombre = "Alonso sánchez Moreno de Vega";
+    System.out.println(nombre);
+    String direccion = "C/Eneas,58";
+    System.out.println(direccion);
+    String telefono = "681 63 40 35";
+    System.out.println(telefono);
+  }
+}
