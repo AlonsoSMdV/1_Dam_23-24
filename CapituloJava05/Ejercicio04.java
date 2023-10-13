aa95fa861080c596ca05a18bac7ff6d0d00f81f7
Alonso <asanmor011004@gmail.com>
2023-10-13 18:32:58 +0200

    Commit

---
+++ CapituloJava05/Ejercicio04.java
@@ -0,0 +1,12 @@
+package CapituloJava05;
+/**
+ * Muestra los números del 320 al 160, contando de 20 en 20 hacia atrás utilizando
+ * un bucle for.
+ */
+public class Ejercicio04 {
+  public static void main(String[] args) {
+    for (int i = 320; i > 159; i-= 20) {
+      System.out.print(i+" ");
+    }
+  }
+}
