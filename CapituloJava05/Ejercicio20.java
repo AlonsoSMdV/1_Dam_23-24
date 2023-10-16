3ba669e3dd3c69d85aae242bb3332fb6958507ea
Alonso <asanmor011004@gmail.com>
2023-10-16 18:27:12 +0200

    Commit

---
+++ CapituloJava05/Ejercicio20.java
@@ -0,0 +1,29 @@
+package CapituloJava05;
+/**
+ * Igual que el ejercicio anterior pero esta vez se debe pintar una pirámide hueca.
+ */
+import java.util.Scanner;
+public class Ejercicio20 {
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+    System.out.print("Introduce la altura de la pirámide: ");
+    int altura = sc.nextInt();
+    System.out.print("Ahora con que caracter quiere pintar la piramide: ");
+    String caracter = sc.next();
+    for (int i = 0; i < altura; i++) { 
+      for (int j = i; j < altura-1; j++) {
+        System.out.print("  ");
+      }
+      
+      for (int j2 = 0; j2 < i*2-1; j2++) {
+        if( i == altura-1|| j2 == 0 || j2 == i*2-2){
+          System.out.print(caracter+" ");
+        }else{
+          System.out.print("  ");
+        }
+      }
+      System.out.println();
+    }
+    sc.close();
+  }
+}
