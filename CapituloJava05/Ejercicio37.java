e2a98459835f3eb2e9fa972b3904cb0d83b176d9
Alonso <asanmor011004@gmail.com>
2023-10-23 10:10:52 +0200

    Commit

---
+++ CapituloJava05/Ejercicio37.java
@@ -0,0 +1,38 @@
+package CapituloJava05;
+/**
+ * Realiza un conversor del sistema decimal al sistema de “palotes”.
+ */
+import java.util.Scanner;
+public class Ejercicio37 {
+  public static long volteado(long n){
+    long nVolt = 0;
+    while(n>0){
+      nVolt = (n%10)+(nVolt*10);
+      n/=10;
+    }
+    return nVolt;
+  }
+
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+    System.out.print("Por favor, introduzca un número entero positivo: ");
+    long n = sc.nextInt();
+    String palotes = "";
+    long volt = volteado(n);
+    long nPalotes = 0;
+    while (volt > 0) {
+      nPalotes = volt%10;
+      for (int i = 0; i < nPalotes; i++) {
+        palotes += "| ";
+      }
+      if (volt>10) {
+        palotes += "- ";
+      }else{
+        palotes += "";
+      }
+      volt /= 10;
+    }
+    System.out.println("El "+n+" en decimal es el "+palotes+" en el sistema de palotes");
+    sc.close();
+  }
+}
