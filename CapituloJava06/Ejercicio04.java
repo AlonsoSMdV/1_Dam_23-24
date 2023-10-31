cde468802e21a4bbd5c5e99182533020f319856e
Alonso <asanmor011004@gmail.com>
2023-10-31 17:23:42 +0100

    Commit

---
+++ CapituloJava06/Ejercicio04.java
@@ -0,0 +1,12 @@
+package CapituloJava06;
+/**
+ * Muestra 20 números enteros aleatorios entre 0 y 10 (ambos incluidos) separados por espacios.
+ */
+public class Ejercicio04 {
+  public static void main(String[] args) {
+    for (int i = 0; i < 20; i++) {
+      int n = (int)(Math.random()*10+1);
+      System.out.print(n+" ");
+    }
+  }
+}
