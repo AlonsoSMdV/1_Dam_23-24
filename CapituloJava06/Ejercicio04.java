a6da240438c150a725ea2ed991fca8ff1db1224e
Alonso <asanmor011004@gmail.com>
2023-11-01 17:01:40 +0100

    Changes

--- CapituloJava06/Ejercicio04.java
+++ CapituloJava06/Ejercicio04.java
@@ -4,7 +4,7 @@ package CapituloJava06;
  */
 public class Ejercicio04 {
   public static void main(String[] args) {
-    for (int i = 0; i < 20; i++) {
+    for (int i = 1; i <= 20; i++) {
       int n = (int)(Math.random()*10+1);
       System.out.print(n+" ");
     }
