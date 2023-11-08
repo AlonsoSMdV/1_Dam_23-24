b23e22e674b23991d5512d0f586805686f3dab47
Alonso <asanmor011004@gmail.com>
2023-11-08 17:17:09 +0100

    Changes

--- CapituloJava06/Ejercicio12.java
+++ CapituloJava06/Ejercicio12.java
@@ -7,7 +7,7 @@ package CapituloJava06;
 public class Ejercicio12 {
   public static void main(String[] args) {
     System.out.println("\033[92m");
-    for (int i = 0; i < 8000; i++) {
+    for (int i = 1; i <= 8000; i++) {
       System.out.print((char)(Math.random()*(126-32+1)+32));
     }
   }
