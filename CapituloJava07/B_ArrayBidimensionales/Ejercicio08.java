1597433610353c66b8725a32997d8784da61be6c
Alonso <asanmor011004@gmail.com>
2023-11-24 11:55:16 +0100

    Changes

--- CapituloJava07/B_ArrayBidimensionales/Ejercicio08.java
+++ CapituloJava07/B_ArrayBidimensionales/Ejercicio08.java
@@ -41,7 +41,7 @@ public class Ejercicio08 {
     System.out.println("El alfill se puede mover por las siguientes posiciones:");
     for (int i = 8; i >= 1; i--) {
       for (int j = 1; j <= 8; j++) {
-        if ((Math.abs(filaAlfil-i) == Math.abs(colAlfil-j)) && ((filaAlfil != i) && (colAlfil != j))) {
+        if ((Math.abs(filaAlfil - i) == Math.abs(colAlfil - j)) && ((filaAlfil != i) && (colAlfil != j))) {
           System.out.print((char)(j+96)+""+i+" ");
         }
       }
