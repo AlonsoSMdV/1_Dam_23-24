4d7c8e11943f455325b8fcf252bf0cc77e17df70
Alonso <asanmor011004@gmail.com>
2023-11-28 18:06:43 +0100

    Changes

--- CapituloJava07/B_ArrayBidimensionales/Ejercicio02.java
+++ CapituloJava07/B_ArrayBidimensionales/Ejercicio02.java
@@ -23,15 +23,15 @@ public class Ejercicio02 {
     System.out.print("Array num| ");
     for (int i = 0; i < 6; i++) {
       if(i< 5){
-        System.out.print(" Columna "+i+" | ");
+        System.out.print(" Columna "+ i +" | ");
       }else{
-        System.out.print(" SumaFila "+i+" | ");
+        System.out.print(" SumaFila "+ i +" | ");
       }
     }
     System.out.println();
     for (int i = 0; i < 4; i++) {
       sumaFila = 0;
-      System.out.print("  Fila "+i+" | ");
+      System.out.print("  Fila "+ i +" | ");
       for (int j = 0; j < 5; j++) {
         System.out.printf("%6d%5s| ",num[i][j]," ");
         sumaFila+=num[i][j];
