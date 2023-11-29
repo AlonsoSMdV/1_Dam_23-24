a57fb8ac998a43cd52f17801405d2ea839ef8551
Alonso <asanmor011004@gmail.com>
2023-11-29 12:09:10 +0100

    Changes

--- CapituloJava07/B_ArrayBidimensionales/Ejercicio08.java
+++ CapituloJava07/B_ArrayBidimensionales/Ejercicio08.java
@@ -8,6 +8,8 @@ package CapituloJava07.B_ArrayBidimensionales;
  */
 import java.util.Scanner;
 public class Ejercicio08 {
+  public static final String RESET = "\033[0m"; // Text Reset
+  public static final String GREEN = "\033[42m"; // GREEN
   public static void main(String[] args) {
     Scanner sc = new Scanner(System.in);
     System.out.print("Introduzca la posicion del alfil: ");
@@ -16,10 +18,22 @@ public class Ejercicio08 {
     int filaAlfil = (int)(posAlfil.charAt(1))-48;
     
     String[][] tablero = new String[8][8];
+    System.out.println("El alfil se puede mover por las siguientes posiciones:");
+    for (int i = 8; i >= 1; i--) {
+      for (int j = 1; j <= 8; j++) {
+        if ((Math.abs(filaAlfil - i) == Math.abs(colAlfil - j)) && ((filaAlfil != i) && (colAlfil != j))) {
+          System.out.print((char)(j+96)+""+i+" ");
+        }
+      }
+    }
+    System.out.println("Aqui te dejo una representacion grafica");
+    System.out.println();
     for (int i = 0; i < 8; i++) {
       for (int j = 0; j < 8; j++) {
         if (i == filaAlfil-1 && j == colAlfil-1) {
           tablero[i][j] = " X │";
+        }else if ((Math.abs((filaAlfil-1) - i) == Math.abs((colAlfil-1) - j)) && (((filaAlfil-1) != i) && ((colAlfil-1) != j))) {
+          tablero[i][j] = GREEN+"   "+RESET+"│";
         }else{
           tablero[i][j] = "   │";
         }
@@ -38,14 +52,7 @@ public class Ejercicio08 {
     }
     System.out.println(" │ a │ b │ c │ d │ e │ f │ g │ h │");
     System.out.println();
-    System.out.println("El alfill se puede mover por las siguientes posiciones:");
-    for (int i = 8; i >= 1; i--) {
-      for (int j = 1; j <= 8; j++) {
-        if ((Math.abs(filaAlfil - i) == Math.abs(colAlfil - j)) && ((filaAlfil != i) && (colAlfil != j))) {
-          System.out.print((char)(j+96)+""+i+" ");
-        }
-      }
-    }
+   
     sc.close();
   }
 }
