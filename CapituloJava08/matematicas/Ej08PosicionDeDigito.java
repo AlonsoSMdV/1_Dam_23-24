cc37e689157aa09ab4e424effc7462b098f9af28
Alonso <asanmor011004@gmail.com>
2023-12-05 16:29:16 +0100

    Commit

---
+++ CapituloJava08/matematicas/Ej08PosicionDeDigito.java
@@ -0,0 +1,21 @@
+package CapituloJava08.matematicas;
+/**
+ * Da la posición de la primera ocurrencia de un dígito
+ * dentro de un número entero. Si no se encuentra, devuelve -1.
+ */
+public class Ej08PosicionDeDigito {
+  public static int posicionDigito(int dig, long n){
+    long volt = Ej06Voltea.voltea(n);
+    int pos = 0;
+    int posDig = -1;
+    while (volt > 0) {
+      if ((volt%10) == dig) {
+        posDig = pos;
+        break;
+      }
+      pos++;
+      volt/=10;
+    }
+    return posDig;
+  }
+}
