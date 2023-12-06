8c9815a2e5b3a56bcbe98fab76c53afcdaf63e3b
Alonso <asanmor011004@gmail.com>
2023-12-06 20:22:37 +0100

    Commit

---
+++ CapituloJava08/matematicas/Ej13TrozoDeNumero.java
@@ -0,0 +1,21 @@
+package CapituloJava08.matematicas;
+/**
+ * Toma como parámetros las posiciones inicial y final
+ * dentro de un número y devuelve el trozo correspondiente.
+ */
+public class Ej13TrozoDeNumero {
+  public static long trozoNumero(long n, int posI, int posF){
+    long volt = Ej06Voltea.voltea(n);
+    int pos = 0;
+    while(pos != posI){
+      volt/=10;
+      pos++;
+    }
+    long resultado = 0;
+    for (int i = posI; i <= posF; i++) {
+      resultado = (volt%10) + (resultado*10);
+      volt/=10;
+    }
+    return resultado;
+  }
+}
