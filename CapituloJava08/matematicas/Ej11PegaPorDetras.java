b1f7504c68a75f4e7b22f52a19b7561b711666be
Alonso <asanmor011004@gmail.com>
2023-12-06 20:22:06 +0100

    Commit

---
+++ CapituloJava08/matematicas/Ej11PegaPorDetras.java
@@ -0,0 +1,10 @@
+package CapituloJava08.matematicas;
+/**
+ *  Añade un dígito a un número por detrás.
+ */
+public class Ej11PegaPorDetras {
+  public static long pegaPorDetras(long n, int dig){
+    return n = (n*10)+dig;
+  }
+
+}
