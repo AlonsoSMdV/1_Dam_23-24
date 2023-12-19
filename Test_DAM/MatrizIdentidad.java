9702986ba7cd98884463a001a3a437a4bb5fcf37
Alonso <asanmor011004@gmail.com>
2023-12-19 16:41:15 +0100

    Changes

--- Test_DAM/MatrizIdentidad.java
+++ Test_DAM/MatrizIdentidad.java
@@ -6,19 +6,19 @@ public class MatrizIdentidad {
   public static void main(String[] args){
     Scanner sc = new Scanner(System.in);
     System.out.println("Diga la anchura de la matriz");
-    int ancho=sc.nextInt();
-    int[][] matriz=new int[ancho][ancho];
-    int diagonal=-1;
-    for(int i=0;i<ancho;i++){
+    int ancho = sc.nextInt();
+    int[][] matriz = new int[ancho][ancho];
+    int diagonal = -1;
+    for(int i = 0; i < ancho; i++){
         diagonal++;
-        for(int j=0;j<ancho;j++){
-            if(j==i){
-                matriz[i][j]=1;
+        for(int j = 0; j < ancho; j++){
+            if(j == i){
+                matriz[i][j] = 1;
             }else{
-                if(i==0 || j==0 || j==ancho-1 || i==ancho-1 || (i==diagonal && j==ancho-diagonal-1)){
-                    matriz[i][j]=1;
+                if(i == 0 || j == 0 || j == ancho-1 || i == ancho-1 || (i == diagonal && j == ancho-diagonal-1)){
+                    matriz[i][j] = 1;
                 }else{
-                    matriz[i][j]=0;
+                    matriz[i][j] = 0;
                   
                 }
             }
