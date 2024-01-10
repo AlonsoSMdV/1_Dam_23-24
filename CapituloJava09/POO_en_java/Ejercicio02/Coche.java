6aeab55ee0e07f519da2c50edc31ecf0fbf629ea
Alonso <asanmor011004@gmail.com>
2024-01-10 17:40:15 +0100

    Commit

---
+++ CapituloJava09/POO_en_java/Ejercicio02/Coche.java
@@ -0,0 +1,12 @@
+package CapituloJava09.POO_en_java.Ejercicio02;
+
+public class Coche extends Vehiculo{
+  int cilindrada;
+  public Coche(int c){
+    this.cilindrada = c;
+  }
+
+  void quemaRueda(){
+    System.out.println("FFfffffffffffffffffsssshhhhhhhhhhhhhhh");
+  }
+}
