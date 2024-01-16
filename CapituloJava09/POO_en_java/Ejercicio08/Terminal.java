a8804c1fb463406a204c53f38ae88e5bfc2a05e5
Alonso <asanmor011004@gmail.com>
2024-01-16 17:13:24 +0100

    Commit

---
+++ CapituloJava09/POO_en_java/Ejercicio08/Terminal.java
@@ -0,0 +1,27 @@
+package CapituloJava09.POO_en_java.Ejercicio08;
+
+public class Terminal {
+  private int duracionLLamada;
+  private String numeroTerminal;
+  public Terminal(String numero) {
+    this.numeroTerminal = numero;
+    this.duracionLLamada = 0;
+  }
+
+  public String getNumeroTerminal() {
+    return numeroTerminal;
+  }
+
+  public int getDuracionLLamada() {
+    return duracionLLamada;
+  }
+
+  public String toString() {
+    return "Nº " + this.numeroTerminal + " - " + this.duracionLLamada + "s de conversación";
+  }
+  public void llama(Terminal t, int duracion) {
+    this.duracionLLamada += duracion;
+    t.duracionLLamada += duracion;
+  }
+  
+}
