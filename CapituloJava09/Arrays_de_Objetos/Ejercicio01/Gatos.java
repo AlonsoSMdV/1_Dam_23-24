e791b6c939be43b1c45e14ee31ded08bc329bc74
Alonso <asanmor011004@gmail.com>
2024-01-24 10:45:04 +0100

    Commit

---
+++ CapituloJava09/Arrays_de_Objetos/Ejercicio01/Gatos.java
@@ -0,0 +1,32 @@
+package CapituloJava09.Arrays_de_Objetos.Ejercicio01;
+
+public class Gatos {
+  private String nombre;
+  private String color;
+  private String sexo;
+
+  public String getNombre() {
+    return nombre;
+  }
+
+  public void setNombre(String nombre) {
+    this.nombre = nombre;
+  }
+  
+  public String getColor() {
+    return color;
+  }
+
+  public void setColor(String color) {
+    this.color = color;
+  }
+
+
+  public String getSexo() {
+    return sexo;
+  }
+
+  public void setSexo(String sexo) {
+    this.sexo = sexo;
+  }
+}
