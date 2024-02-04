f7a0d7d415c83e8ff5f948c6fb8fb88c96d8ceee
Alonso <asanmor011004@gmail.com>
2024-02-04 18:48:49 +0100

    Commit

---
+++ CapituloJava10/Ejercicios/Ejercicio09/Ejercicio09.java
@@ -0,0 +1,24 @@
+package CapituloJava10.Ejercicios.Ejercicio09;
+
+import java.util.ArrayList;
+import java.util.Collections;
+
+public class Ejercicio09 {
+  public static void main(String[] args) {
+    ArrayList<Carta> c = new ArrayList<Carta>();
+    Carta cartaAux = new Carta();
+    c.add(cartaAux);
+    for (int i = 0; i < 9; i++) {
+      do {
+        cartaAux = new Carta();
+      } while (c.contains(cartaAux));
+
+      c.add(cartaAux);
+    }
+    Collections.sort(c);
+    
+    for (Carta ca : c) {
+      System.out.println(ca);
+    }
+  }
+}
