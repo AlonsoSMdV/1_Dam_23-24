cddb2f9a959fd55740d8194da41c6d450dc13993
Alonso <asanmor011004@gmail.com>
2024-02-19 08:19:23 +0100

    Commit

---
+++ CapituloJava11/Ejercicio03.java
@@ -0,0 +1,45 @@
+package CapituloJava11;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+
+public class Ejercicio03 {
+    public static void main(String[] args) {
+    if (args.length != 3) {
+      System.out.println("Uso del programa: java .\\Ejercicio03.java FICHERO1.txt FICHERO2.txt MEZCLA.txt ");
+      System.exit(-1); // sale del programa
+    }
+    try {
+      BufferedReader bf1 = new BufferedReader(new FileReader(args[0]));
+      BufferedReader bf2 = new BufferedReader(new FileReader(args[1]));
+      BufferedWriter bw = new BufferedWriter(new FileWriter(args[2]));
+    
+      String linea1 = "";
+      String linea2 = "";
+      
+      while ( (linea1 != null) || (linea2 != null) ) {
+        linea1 = bf1.readLine();
+        linea2 = bf2.readLine();
+        
+        if (linea1 != null) {
+          bw.write(linea1 + "\n");
+        }
+        
+        if (linea2 != null) {
+          bw.write(linea2 + "\n");
+        }
+      } 
+    
+      bf1.close();
+      bf2.close();
+      bw.close();
+    
+    } catch (IOException ioe) {
+      System.out.println("Se ha producido un error de lectura/escritura");
+      System.err.println(ioe.getMessage());
+    }
+  }
+}
